3e39b62da98fb21f4e1ab52a0ab3af98f390ceb8
Luis Felipe de Almeida da Silva <luisfelipeas5@gmail.com>
2015-05-09 23:14:42 -0300

    Update Leitura_Arquivo.java
    Exclusão de alguns imports que não foram utilizados e

--- Leitura_Arquivo.java
+++ Leitura_Arquivo.java
@@ -1,6 +1,4 @@
-import Jama.Matrix; 
 import java.io.FileNotFoundException; 
-import java.lang.IndexOutOfBoundsException; 
 import java.io.FileReader;
 import java.util.Scanner; 
 import java.util.NoSuchElementException; 
@@ -13,14 +11,15 @@ Serve apenas para ler o arquivo de texto. Para isso, contem o metodo obtem_dados
 objeto do tipo Scanner. Este metodo retorna um objeto da classe Situacao_problema, que eh usada para compactar as matrizes 
 de entrada e de saida esperada. */
 public class Leitura_Arquivo {
-	public static Situacao_problema obtem_dados (String arquivo) {
+	public static Situacao_Problema obtem_dados (String arquivo) {
 		double[][] matriz_dados;
 		double[][] saida;
-		Situacao_problema sit = null; 
+		Situacao_Problema sit = null; 
 		LinkedList<String> linhas = new LinkedList<String>(); 
 		int num_instancias = 0; 
+		Scanner input=null;
 		try {
-			Scanner input = new Scanner (new FileReader(arquivo)); 
+			input = new Scanner (new FileReader(arquivo)); 
 			while (true) {
 				String aux = input.nextLine();
 				linhas.add(aux); 
@@ -49,8 +48,9 @@ public class Leitura_Arquivo {
 				}
 				saida[i][0] = Double.parseDouble(l_aux[num_atributos]); 
 			}
-			sit = new Situacao_problema (matriz_dados, saida); 
+			sit = new Situacao_Problema (matriz_dados, saida); 
 		}
+		input.close();
 		return sit; 
 	}
 }
