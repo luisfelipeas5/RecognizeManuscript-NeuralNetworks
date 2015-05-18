35139188b97cf8878c6a4aa89bdb362b100edb45
Luis Felipe A.S. <luisfelipeas@LF-Book>
2015-05-17 22:49:52 -0300

    -Modularização do treinamento para uma epoca; e -Alteração da condicao de parada para o treinamento, comparando o erro usando o conjunto de validacao e o conjunto de treinamento

--- Testa_Treinamento.java
+++ Testa_Treinamento.java
@@ -2,22 +2,33 @@ import Jama.Matrix;
 
 public class Testa_Treinamento {
 	public static void main(String[] args) {
-		Treinamento treinamento=new Treinamento();
 		double[][] array_entrada= 	{{1, 1, 1},
 									{ 1, 1, 1 }};
-		Matrix entrada=new Matrix(array_entrada);
+		Matrix entradas=new Matrix(array_entrada);
 		
 		double[][] array_saida_desejada= {{ 1 },
 										 { 1 }};
-		Matrix saida_desejada=new Matrix(array_saida_desejada);
+		Matrix saidas_desejadas=new Matrix(array_saida_desejada);
 		
 		int numero_neuronios_escondidos=2;
-		int epocas=2;
-		
+		//int epocas=2; 
+		/*
+		o numero de epocas na verdade eh definido no ponto de interseccao quando:
+		validacao.erro==treinamento.erro
+		*/
 		//treinando padrao a padrao:
 		boolean treina_padrao_padrao=true;
 		Rede mlp=new MLP( numero_neuronios_escondidos, treina_padrao_padrao);
-		treinamento.treina(entrada, saida_desejada, numero_neuronios_escondidos, epocas, mlp);
+		
+		//70% treinamento 30% validacao
+		double validacao=0.3;
+		int num_linhas_validacao= (int) (entradas.getRowDimension() * validacao);
+		Matrix entradas_validacao=entradas.getMatrix(entradas.getRowDimension()-num_linhas_validacao, num_linhas_validacao-1,0, entradas.getColumnDimension()) ;
+		Matrix saidas_desejadas_validacao=saidas_desejadas.getMatrix(entradas.getRowDimension()-num_linhas_validacao, num_linhas_validacao-1, 0, entradas.getColumnDimension()) ;;
+		
+		//Um objeto treinamento para cada tipo de rede
+		Treinamento treinamento=new Treinamento(mlp);
+		treinamento.treina(entradas, saidas_desejadas, entradas_validacao, saidas_desejadas_validacao);
 	}
 }
 
--- Treinamento.java
+++ Treinamento.java
@@ -2,63 +2,93 @@ import java.util.Random;
 import Jama.Matrix;
 
 public class Treinamento {
-	public void treina_mlp(Matrix entrada, Matrix saida_desejada, int epocas, Rede rede) {
-		//Para a primeira epoca, os pesos devem ser gerados randomicamente
+	
+	Rede rede;
+	Matrix pesos_a;
+	Matrix pesos_b;
+	
+	public Treinamento(Rede rede) {
+		this.rede=rede;
+	}
+
+	public double treina_epoca(Matrix entradas, Matrix saidas_desejadas, boolean treina_batelada) {
 		
-		int linhas_pesos_a = rede.numero_neuronios_escondidos;
-		int colunas_pesos_a = entrada.getColumnDimension();
-		double[][] array_pesos_a=new double[ linhas_pesos_a ][ colunas_pesos_a ];
-		this.gera_pesos_aleatorios(array_pesos_a);
-		Matrix pesos_a=new Matrix( array_pesos_a);
+		Matrix entrada; //instância de entrada
+		Matrix saida_desejada; //saida deseja da instância de entrada
+		Matrix erros=new Matrix(saidas_desejadas.getRowDimension(),	1); //matrix com cada erro quadrado de cada instancia
 		
-		int linhas_pesos_b = 1; //numero de neuronios de saida: nessa MLP em especifico, um unico neuronio de saida
-		int colunas_pesos_b = rede.numero_neuronios_escondidos+1; //Mais o bias
-		double[][] array_pesos_b=new double[ linhas_pesos_b ][ colunas_pesos_b ];
-		this.gera_pesos_aleatorios(array_pesos_b);
-		Matrix pesos_b=new Matrix( array_pesos_b);
-		
-		Matrix saidas=new Matrix(1, rede.numero_neuronios_saida);
-		for (int i = 0; i < entrada.getRowDimension(); i++) {
-			Matrix saida=rede.calcula_saida(entrada, saida_desejada, pesos_a, pesos_b);
-			saidas.setMatrix(new int [] {i}, new int[] {1}, saida );
-		}
-		
-		double erro_total=this.calcula_erro_total(saida_desejada, saidas);	
-		
-		double erro_desejado=0.05;//Deifinir qual seria uma taxa de erro razoavel
-		for( int epoca_atual=1; epoca_atual<epocas && erro_total>erro_desejado; epoca_atual++ ) {
-			/*Caso a atualizacao de erro seja a batelada
-			//calcular Gradiente
-			//Matrix gradiente=this.calculaGradiente();
-			//calcular Passo
-			//double alpha=this.calculaPasso();
-			//Atualizar pesos
-			//this.atualizaPesos(pesosA,alpha);
-			//this.atualizaPesos(pesosB,alpha);
-			*/
-			for (int i = 0; i < entrada.getRowDimension(); i++) {
-				Matrix saida=rede.calcula_saida(entrada, saida_desejada, pesos_a, pesos_b);
-				saidas.setMatrix(new int [] {i}, new int[] {1}, saida );
+		//iteracao sobre todas as instancias do conjunto de entrada
+		for(int i=0; i<entradas.getRowDimension(); i++) {
+			
+			//set dos conjuntos entrada e saida_desejada
+			entrada=entradas.getMatrix(0, 0, 0, entradas.getColumnDimension()-1);
+			saida_desejada=saidas_desejadas.getMatrix(0, 0, 0, entradas.getColumnDimension()-1);
+			
+			//passa para rede em questao o conjunto a instancia de entrada e recebe a saida
+			Matrix saida=this.rede.calcula_saida(entrada, saida_desejada, pesos_a, pesos_b);
+			
+			//calcula o erro a partir da saida desejada da instancia de entrada e a saida da rede
+			//Caso a rede seja uma MLP, essa matriz tera um unico elemento,
+			//Caso a rede seja uma LVQ, essa matriz tera um elemento para cada neuronio de saida
+			Matrix erro_matrix=saida_desejada.minus(saida);		
+			double erro=0; //erro quadrado da instancia
+			for(int j=0; j<erro_matrix.getColumnDimension(); j++) {
+				double erro_intancia=erro_matrix.get(0, j);
+				erro=erro+(erro_intancia*erro_intancia);
 			}
 			
-			double erro_total_antigo=erro_total;
-			erro_total=this.calcula_erro_total(saida_desejada, saidas);
-			while (erro_total>erro_total_antigo) {
-				//TODO: calcular o erro_total ajustando o passo
-				erro_total=erro_total/2;
-			}
+			erros.set(i, 0, erro);//erro quadrado da instancia eh adicionado a matriz de erros
 		}
 		
-		int casas_decimais=3;
-		System.out.println("Pesos A");
-		pesos_a.print(colunas_pesos_a, casas_decimais);
-		System.out.println("Pesos B");
-		pesos_b.print(colunas_pesos_b, casas_decimais);
-		System.out.println("Saida");
-		saidas.print(saidas.getColumnDimension(), casas_decimais);
-		System.out.println("Saida Desejada");
-		saida_desejada.print(saida_desejada.getColumnDimension(), casas_decimais);
+		double erro_epoca=0; //erro quadratico total da epoca
+		for(int i=0; i<erros.getRowDimension(); i++) {
+			erro_epoca=erro_epoca+erros.get(i, 0);
+		}
 		
+		if(treina_batelada) {
+			//TODO
+			/*
+			double erro_medio=erro/entrada.getRowDimension();
+			rede.atualiza_pesos(erro, this.pesos_a, this.pesos_b, taxa_aprendizado);
+			*/
+		}
+		return erro_epoca;
+	}
+	
+
+	public void treina(Matrix entradas_treinamento, Matrix saidas_desejadas_treinamento,
+			Matrix entradas_validacao, Matrix saidas_desejadas_validacao) {
+		//Para a primeira epoca, os pesos devem ser gerados randomicamente
+		
+		Matrix pesos_a= new Matrix( rede.numero_neuronios_escondidos, entradas_treinamento.getColumnDimension() );
+		Matrix pesos_b= new Matrix( saidas_desejadas_treinamento.getColumnDimension(), rede.numero_neuronios_escondidos );
+		this.gera_pesos_aleatorios(pesos_a);
+		this.gera_pesos_aleatorios(pesos_b);
+		
+		double erro_total_treinamento=1.0;
+		double erro_total_validacao=-1.0;
+		
+		while (erro_total_treinamento!=erro_total_validacao) {
+			//erro total para o conjunto de treinamento
+			boolean treina_batelada= ! rede.treina_padrao_padrao;
+			erro_total_treinamento= treina_epoca(entradas_treinamento, saidas_desejadas_treinamento, treina_batelada);
+			/*erro total para o conjunto de validacao
+				*nao eh feita nenhum tipo de atualizacao de pesos, por isso
+				*a rede nao pode atualizar padrao a padrao, nem mesmo em batch 
+			*/		
+			treina_batelada=false;
+			boolean treina_padrao_padrao_old=rede.treina_padrao_padrao; //armazena o antigo tipo de treinamento da rede antes de mudar 
+			rede.treina_padrao_padrao=false;
+			erro_total_validacao= treina_epoca(entradas_validacao, saidas_desejadas_validacao, treina_batelada);
+			rede.treina_padrao_padrao=treina_padrao_padrao_old;
+		
+			/* Printar os pesos a cada iteracao */
+			int casas_decimais=3;
+			System.out.println("Pesos A");
+			pesos_a.print(pesos_a.getColumnDimension(), casas_decimais);
+			System.out.println("Pesos B");
+			pesos_b.print(pesos_b.getColumnDimension(), casas_decimais);
+		}
 	}
 	
 	
@@ -129,11 +159,11 @@ public class Treinamento {
 	 * Esse metodo, dado uma matriz pesos de dimensoes quaisquer, preenche pesos com valores aleatorios
 	 * entre 0.0 e 1.0 
 	 */
-	public void gera_pesos_aleatorios(double[][] pesos) {
+	public void gera_pesos_aleatorios(Matrix pesos) {
 		Random random=new Random();
-		for(int i=0; i<pesos.length; i++) {
-			for(int j=0; j<pesos[i].length; j++) {
-				pesos[i][j]=random.nextDouble();
+		for(int i=0; i< pesos.getRowDimension(); i++) {
+			for(int j=0; j<pesos.getColumnDimension();i++) {
+				pesos.set(i, j, random.nextDouble());
 			}
 		}
 	}
