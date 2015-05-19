3d54cf4540e33fc3495e8babc4fdc0df87453f5b
Luis Felipe A.S. <luisfelipeas@LF-Book>
2015-05-18 23:28:01 -0300

    Atualização da classe abstrata Rede  - metodo calcula saida retorna o erro quadratico dado todo o conjunto de entradas e saidas desejadas

--- Rede.java
+++ Rede.java
@@ -4,34 +4,23 @@ import Jama.Matrix;
 abstract class Rede {
 	int numero_neuronios_escondidos; // No caso da LVQ = 0 
 	boolean treina_padrao_padrao; 
+	boolean treina_batelada;
 	
-	//Matrix entrada; 
-	//Matrix saida_desejada; 
-	//Matrix[] matrizes_pesos;
-	//Matrix[] saidas_rede;
-	//int numero_neuronios_saida; = saida.getColumnDimension() // Varia de acordo com a codificacao da saida desejada
-	//int numero_entradas; = entrada.getColumnDimension()
-	//int numero_saidas; = saida.getColumnDimension()
-	double taxa_aprendizado;
-	
-	Rede (int numero_neuronios_escondidos, boolean treina_padrao_padrao){
+	Rede (int numero_neuronios_escondidos, boolean treina_padrao_padrao, boolean treina_batelada){
 		this.numero_neuronios_escondidos=numero_neuronios_escondidos;
 		this.treina_padrao_padrao=treina_padrao_padrao;
-		/*
-		double[][] s = saida.getArrayCopy(); 
-		double[][] e = entrada.getArrayCopy(); 
-		this.numero_entradas = e[0].length; 
-		this.numero_saidas = s[0].length;
-		*/
+		this.treina_batelada=treina_batelada;
 	}
 	
 	/*
-	 *Esse metodo calcula a saida dessa rede para uma instancia somente, 
-	 *dado duas matrizes de peso  
+	 *Esse metodo retorna o erro quadratico total da epoca, dado
+	 *- um conjunto de entradas
+	 *- um conjunto de saidas desejadas
 	 */
-	abstract Matrix calcula_saida(Matrix entrada, Matrix saida_desejada, Matrix pesos_a, Matrix pesos_b);
+	abstract double calcula_saida(Matrix entradas, Matrix saida_desejadas, Matrix pesos_a, Matrix pesos_b);
 	/*
 	 * Esse metodo atualiza as matrizes de pesos dado um erro
+	 * manipulado pela classe calcula_saida
 	 */
 	abstract void atualiza_pesos(double erro, Matrix pesos_a, Matrix pesos_b, Matrix entrada );
 }
