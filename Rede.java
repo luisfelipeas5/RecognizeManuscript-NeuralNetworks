912b3cdf094e2a3decc68d7452177d0ceb29eb2b
AntonioMateus <antoniocms1995@hotmail.com>
2015-05-31 21:44:43 -0300

    Rede comentada de acordo com o Javadoc

--- Rede.java
+++ Rede.java
@@ -1,5 +1,6 @@
 import Jama.Matrix;
 
+/**Classe abstrata cujos metodos devem ser implementados pelas classes que a extendem (MLP e LVQ).*/
 abstract class Rede {
 	int numero_neuronios; // No caso da LVQ = 0
 	boolean necessidade_atualizar_pesos=true;
@@ -8,22 +9,18 @@ abstract class Rede {
 		this.numero_neuronios=numero_neuronios; 
 	}
 	
-	/*
-	 *Esse metodo retorna o erro quadratico total da epoca, dado
-	 *- um conjunto de entradas
-	 *- um conjunto de saidas desejadas
-	 */
-	//abstract void calcula_saida(Matrix entrada, Matrix saida_desejadas, );
-	/*
-	 * Esse metodo atualiza as matrizes de pesos dado um erro
-	 * manipulado pela classe calcula_saida
-	 */
-	//abstract void atualiza_pesos(Matrix entrada, Matrix saida, Matrix pesos_a, Matrix pesos_b);
+	/**Retorna o erro obtido durante a execucao de uma epoca completa. */
 	abstract double get_erro(); 
+	/**Retorna todas as saidas geradas pela rede durante uma epoca*/
 	abstract Matrix get_saidas(); 
+	/**Informa a rede qual o modo de treinamento desejado. A codificacao eh 1 para o treinamento 
+	padrao a padrao e 2 para o em batelada */
 	abstract void set_modo_treinamento (int modo_treinamento); 
+	/**Passa para a rede as matrizes de pesos iniciais*/
 	abstract void set_pesos (Matrix pesos_a, Matrix pesos_b); 
+	/**Passa para a rede as matrizes que contem as entradas e as correspondentes saidas. */
 	abstract void set_problema (Matrix entrada, Matrix saida_desejada); 		
+	/** Inverte o valor da variavel booleana que indica se a rede precisa atualizar as matrizes de pesos. */
 	void set_necessidade_atualizacao() {
 		necessidade_atualizar_pesos=!necessidade_atualizar_pesos;
 	}
